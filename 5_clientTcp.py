7701739cfef8a274dc420b90924e320cd82865f2
ShakshiPandey77 <49754283+ShakshiPandey77@users.noreply.github.com>
2019-11-27 22:38:30 +0530

    Update 5_clientTcp.py

--- 5_clientTcp.py
+++ 5_clientTcp.py
@@ -3,7 +3,7 @@ serverName=gethostname()
 serverPort=12001
 clientSocket=socket(AF_INET,SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
-file1=input("Enter file name and the word : ")
+file1=input("Enter file name and the word : ") #create some txt file on desktop with any 2 works repeating and enter eg. file1.txt hello
 clientSocket.send(file1.encode())
 wordcount=clientSocket.recv(1024).decode('utf8')
 print(wordcount)
