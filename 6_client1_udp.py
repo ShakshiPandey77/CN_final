7a871dc50ad13d495f40cc705f59b98299f3512c
ShakshiPandey77 <49754283+ShakshiPandey77@users.noreply.github.com>
2019-11-27 22:35:22 +0530

    Update 6_client1_udp.py

--- 6_client1_udp.py
+++ 6_client1_udp.py
@@ -2,7 +2,7 @@ from socket import *
 serverName = '127.0.0.1'
 serverPort = 12000
 clientSocket = socket(AF_INET,SOCK_DGRAM)
-sentence = input("Enter a word : ") /* enter the text present among those 5 files */
+sentence = input("Enter a word : ")  #enter the text present among those 5 files 
 clientSocket.sendto(bytes(sentence,"utf-8"),(serverName,serverPort))
 filecontents,aerverAddress = clientSocket.recvfrom(2048)
 print("from server : ",filecontents.decode('utf-8'))
